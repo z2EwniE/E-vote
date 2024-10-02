121733a288b6ba1c8bad93d6b7f451244ac408ec
calvskie <shadow>
2024-10-03 00:26:10 +0800

    Tabbel lang

--- login.php
+++ login.php
@@ -33,43 +33,43 @@ if ($login->isRememberSet()) {
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Login | <?= APP_NAME ?></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/my-login.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/logins/login-9/assets/css/login-9.css">
-    <!--    Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css"/>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Login | <?= APP_NAME ?></title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+            <link rel="stylesheet" href="assets/css/my-login.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+            <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/logins/login-9/assets/css/login-9.css">
+            <!--    Font Awesome -->
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css"/>
 
 
-    
-</head>
-   
-    <body class="my-login-page">
-    <section class="bg-primary py-3 py-md-5 py-xl-8">
-    
-    <div class="container">
-    <div class="row gy-4 align-items-center">
-      <div class="col-12 col-md-6 col-xl-7">
-        <div class="d-flex justify-content-center text-bg-primary">
-          <div class="col-12 col-xl-9">
-          <h2 class="h1 mb-4">We make digital products that drive you to stand out.</h2>
-            <p class="lead mb-5">We write words, take photos, make videos, and interact with artificial intelligence.</p>
-            <div class="text-endx">
-              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-grip-horizontal" viewBox="0 0 16 16">
-                <path d="M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-              </svg>
-              </div>
-          </div>
-        </div>
-      </div>
+            
+        </head>
+        
+            <body class="my-login-page">
+            <section class="bg-primary py-3 py-md-5 py-xl-8">
+            
+            <div class="container">
+            <div class="row gy-4 align-items-center">
+            <div class="col-12 col-md-6 col-xl-7">
+                <div class="d-flex justify-content-center text-bg-primary">
+                <div class="col-12 col-xl-9">
+                <h2 class="h1 mb-4">We make digital products that drive you to stand out.</h2>
+                    <p class="lead mb-5">We write words, take photos, make videos, and interact with artificial intelligence.</p>
+                    <div class="text-endx">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-grip-horizontal" viewBox="0 0 16 16">
+                        <path d="M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                    </div>
+                </div>
+                </div>
+            </div>
                         <?php if (!$login->isRememberSet()): ?>
                             
                             <div class="col-12 col-md-6 col-xl-5">
