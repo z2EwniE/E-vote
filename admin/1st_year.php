fe4427967ceab6357a81a477873561e30e0b2957
calvskie <shadow>
2024-10-04 08:56:48 +0800

    trying on goofy branch

---
+++ admin/1st_year.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="author" content="qoshima">
+	<meta name="keywords" content=" Admin, dashboard, responsive, css, sass, html, theme, front-end">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+	<title>E-Vote System</title>
+
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+
+	<!-- Choose your prefered color scheme -->
+	 <link href="css/light.css" rel="stylesheet">
+     </head>
+
+
+        <body data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-layout="default">
+            <div class="wrapper">
+
+                <?php
+                    include_once 'includes/sidebar.php';
+                ?>
+
+                <div class="main">
+
+                    <?php
+                        include_once 'includes/navbar.php';
+                    ?>
+                    
