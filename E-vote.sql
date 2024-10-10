842efea638fdb325f76c7899640e758edfab444f
calvskie <shadow>
2024-10-11 00:10:33 +0800

    sql only

--- E-vote.sql
+++ E-vote.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 03, 2024 at 01:05 PM
--- Server version: 10.4.32-MariaDB
+-- Generation Time: Oct 10, 2024 at 03:45 PM
+-- Server version: 11.4.3-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -40,6 +40,27 @@ CREATE TABLE `candidates` (
 
 INSERT INTO `candidates` (`candidate_id`, `candidate_position`, `candidate_name`, `department`) VALUES
 (1, 0, 'Romel Moster', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL,
+  `course_department` int(11) NOT NULL,
+  `course_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `course_department`, `course_name`) VALUES
+(1, 1, 'Bachelor of Science in Information Technology'),
+(2, 1, 'Bachelor of Science in English Language'),
+(3, 1, 'Bachelor of Science in Social Sciences');
 
 -- --------------------------------------------------------
 
@@ -195,7 +216,7 @@ CREATE TABLE `students` (
   `middle_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `suffix_name` varchar(255) NOT NULL,
-  `course` varchar(10) NOT NULL,
+  `course` int(10) NOT NULL,
   `year_level` varchar(10) NOT NULL,
   `department` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
@@ -205,126 +226,7 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `student_id`, `first_name`, `middle_name`, `last_name`, `suffix_name`, `course`, `year_level`, `department`) VALUES
-(1, 'E21-00955', 'Calvin Andrei', 'Caguioa', 'Flores', 'N/A', 'BSIT', '4', 1),
-(2, 'E21-00272', 'Romel', 'Candilario', 'Moster', '', 'BSIT', '4', 1),
-(4, 'E21-00273', 'Robert', 'Camanzi', 'Estrada', '', 'BSIT', '4', 1),
-(5, 'E21-00273', 'John', 'David', 'Santiago', '', 'BSIT', '4', 1),
-(6, 'E21-00274', 'Maria', 'Angel', 'Cruz', '', 'BSIT', '4', 1),
-(7, 'E22-00278', 'Daniel', 'Joseph', 'Mendoza', '', 'BSIT', '3', 1),
-(8, 'E22-00279', 'Sophia', 'Grace', 'Villanueva', '', 'BSIT', '3', 1),
-(9, 'E22-00280', 'Joshua', 'Luis', 'Hernandez', '', 'BSIT', '3', 1),
-(10, 'E22-00281', 'Isabella', 'Marie', 'Santos', '', 'BSIT', '3', 1),
-(11, 'E22-00282', 'Benjamin', 'Francis', 'De Guzman', '', 'BSIT', '3', 1),
-(12, 'E23-00283', 'Elijah', 'Andrew', 'Ramirez', '', 'BSIT', '2', 1),
-(13, 'E23-00284', 'Chloe', 'Mae', 'Bautista', '', 'BSIT', '2', 1),
-(14, 'E23-00285', 'Ethan', 'Gabriel', 'Navarro', '', 'BSIT', '2', 1),
-(15, 'E23-00286', 'Olivia', 'Rose', 'Aquino', '', 'BSIT', '2', 1),
-(16, 'E23-00287', 'Lucas', 'Xavier', 'Fernandez', '', 'BSIT', '2', 1),
-(22, 'E24-00288', 'Samuel', 'Isaiah', 'Morales', '', 'BSIT', '1', 1),
-(23, 'E24-00289', 'Mia', 'Claire', 'Rodriguez', '', 'BSIT', '1', 1),
-(24, 'E24-00290', 'Aiden', 'Thomas', 'Del Rosario', '', 'BSIT', '1', 1),
-(25, 'E24-00291', 'Emily', 'Jade', 'Vergara', '', 'BSIT', '1', 1),
-(26, 'E24-00292', 'James', 'Alexander', 'Castillo', '', 'BSIT', '1', 1),
-(27, 'E21-00294', 'Sarah', 'Elizabeth', 'Ramirez', '', 'BSMATH', '4', 1),
-(28, 'E21-00295', 'Nathan', 'Oliver', 'Cruz', '', 'BSMATH', '4', 1),
-(29, 'E21-00296', 'Lily', 'Anne', 'Mendoza', '', 'BSMATH', '4', 1),
-(30, 'E21-00297', 'Daniel', 'Christopher', 'Reyes', '', 'BSMATH', '4', 1),
-(31, 'E21-00298', 'Emma', 'Kate', 'Flores', '', 'BSMATH', '4', 1),
-(32, 'E22-00293', 'Kevin', 'James', 'Gonzales', '', 'BSMATH', '3', 1),
-(33, 'E22-00294', 'Grace', 'Anne', 'Bautista', '', 'BSMATH', '3', 1),
-(34, 'E22-00295', 'Miguel', 'Angel', 'Lim', '', 'BSMATH', '3', 1),
-(35, 'E22-00296', 'Vanessa', 'Joy', 'Santos', '', 'BSMATH', '3', 1),
-(36, 'E22-00297', 'Christopher', 'Ryan', 'Morales', '', 'BSMATH', '3', 1),
-(42, 'E23-00288', 'Olivia', 'Marie', 'Reyes', '', 'BSMATH', '2', 1),
-(43, 'E23-00289', 'Lucas', 'James', 'Torres', '', 'BSMATH', '2', 1),
-(44, 'E23-00290', 'Mia', 'Claire', 'Valdez', '', 'BSMATH', '2', 1),
-(45, 'E23-00291', 'Noah', 'Daniel', 'Fernandez', '', 'BSMATH', '2', 1),
-(46, 'E23-00292', 'Emma', 'Rose', 'Santos', '', 'BSMATH', '2', 1),
-(47, 'E24-00293', 'Aiden', 'Leo', 'Ramirez', '', 'BSMATH', '1', 1),
-(48, 'E24-00294', 'Sophia', 'Jade', 'Villanueva', '', 'BSMATH', '1', 1),
-(49, 'E24-00295', 'Ethan', 'Paul', 'Cruz', '', 'BSMATH', '1', 1),
-(50, 'E24-00296', 'Ava', 'Lynn', 'Santos', '', 'BSMATH', '1', 1),
-(51, 'E24-00297', 'James', 'Kyle', 'Torres', '', 'BSMATH', '1', 1),
-(52, 'E21-00300', 'Daniel', 'Joseph', 'Mendoza', '', 'BAEL', '4', 1),
-(53, 'E21-00301', 'Isabella', 'Marie', 'Santos', '', 'BAEL', '4', 1),
-(54, 'E21-00302', 'Samuel', 'Isaiah', 'Morales', '', 'BAEL', '4', 1),
-(55, 'E21-00303', 'Emily', 'Claire', 'Rodriguez', '', 'BAEL', '4', 1),
-(56, 'E21-00304', 'Benjamin', 'Andrew', 'Cruz', '', 'BAEL', '4', 1),
-(57, 'E22-00300', 'Mia', 'Kate', 'Villanueva', '', 'BAEL', '3', 1),
-(58, 'E22-00301', 'Elijah', 'Thomas', 'Ramirez', '', 'BAEL', '3', 1),
-(59, 'E22-00302', 'Grace', 'Anne', 'Bautista', '', 'BAEL', '3', 1),
-(60, 'E22-00303', 'Aiden', 'Lucas', 'Fernandez', '', 'BAEL', '3', 1),
-(61, 'E22-00304', 'Noah', 'Paul', 'Torres', '', 'BAEL', '3', 1),
-(62, 'E23-00300', 'Lucas', 'James', 'Gonzales', '', 'BAEL', '2', 1),
-(63, 'E23-00301', 'Olivia', 'Rose', 'Aquino', '', 'BAEL', '2', 1),
-(64, 'E23-00302', 'Sophia', 'Claire', 'Valdez', '', 'BAEL', '2', 1),
-(65, 'E23-00303', 'Ethan', 'Gabriel', 'Navarro', '', 'BAEL', '2', 1),
-(66, 'E23-00304', 'Emma', 'Joy', 'Santos', '', 'BAEL', '2', 1),
-(67, 'E24-00300', 'Kevin', 'Michael', 'Reyes', '', 'BAEL', '1', 1),
-(68, 'E24-00301', 'Vanessa', 'Joy', 'Morales', '', 'BAEL', '1', 1),
-(69, 'E24-00302', 'Aiden', 'Leo', 'Lim', '', 'BAEL', '1', 1),
-(70, 'E24-00303', 'Daniel', 'Christopher', 'Santos', '', 'BAEL', '1', 1),
-(71, 'E24-00304', 'Sarah', 'Elizabeth', 'Cruz', '', 'BAEL', '1', 1),
-(72, 'E21-00300', 'Daniel', 'Joseph', 'Mendoza', '', 'BPA', '4', 1),
-(73, 'E21-00301', 'Isabella', 'Marie', 'Santos', '', 'BPA', '4', 1),
-(74, 'E21-00302', 'Samuel', 'Isaiah', 'Morales', '', 'BPA', '4', 1),
-(75, 'E21-00303', 'Emily', 'Claire', 'Rodriguez', '', 'BPA', '4', 1),
-(76, 'E21-00304', 'Benjamin', 'Andrew', 'Cruz', '', 'BPA', '4', 1),
-(77, 'E22-00300', 'Mia', 'Kate', 'Villanueva', '', 'BPA', '3', 1),
-(78, 'E22-00301', 'Elijah', 'Thomas', 'Ramirez', '', 'BPA', '3', 1),
-(79, 'E22-00302', 'Grace', 'Anne', 'Bautista', '', 'BPA', '3', 1),
-(80, 'E22-00303', 'Aiden', 'Lucas', 'Fernandez', '', 'BPA', '3', 1),
-(81, 'E22-00304', 'Noah', 'Paul', 'Torres', '', 'BPA', '3', 1),
-(82, 'E23-00300', 'Lucas', 'James', 'Gonzales', '', 'BPA', '2', 1),
-(83, 'E23-00301', 'Olivia', 'Rose', 'Aquino', '', 'BPA', '2', 1),
-(84, 'E23-00302', 'Sophia', 'Claire', 'Valdez', '', 'BPA', '2', 1),
-(85, 'E23-00303', 'Ethan', 'Gabriel', 'Navarro', '', 'BPA', '2', 1),
-(86, 'E23-00304', 'Emma', 'Joy', 'Santos', '', 'BPA', '2', 1),
-(87, 'E24-00300', 'Kevin', 'Michael', 'Reyes', '', 'BPA', '1', 1),
-(88, 'E24-00301', 'Vanessa', 'Joy', 'Morales', '', 'BPA', '1', 1),
-(89, 'E24-00302', 'Aiden', 'Leo', 'Lim', '', 'BPA', '1', 1),
-(90, 'E24-00303', 'Daniel', 'Christopher', 'Santos', '', 'BPA', '1', 1),
-(91, 'E24-00304', 'Sarah', 'Elizabeth', 'Cruz', '', 'BPA', '1', 1),
-(92, 'E21-00300', 'Michael', 'Anthony', 'Santos', '', 'BAP', '4', 1),
-(93, 'E21-00301', 'Charlotte', 'Lee', 'Garcia', '', 'BAP', '4', 1),
-(94, 'E21-00302', 'David', 'Aaron', 'Cruz', '', 'BAP', '4', 1),
-(95, 'E21-00303', 'Sophia', 'Jane', 'Villanueva', '', 'BAP', '4', 1),
-(96, 'E21-00304', 'William', 'Jacob', 'Reyes', '', 'BAP', '4', 1),
-(97, 'E22-00300', 'Emma', 'Lynn', 'Morales', '', 'BAP', '3', 1),
-(98, 'E22-00301', 'Liam', 'George', 'Bautista', '', 'BAP', '3', 1),
-(99, 'E22-00302', 'Ava', 'Claire', 'Mendoza', '', 'BAP', '3', 1),
-(100, 'E22-00303', 'Noah', 'Michael', 'Ramirez', '', 'BAP', '3', 1),
-(101, 'E22-00304', 'Isabella', 'Rose', 'Aquino', '', 'BAP', '3', 1),
-(102, 'E23-00300', 'Lucas', 'Ethan', 'Gonzales', '', 'BAP', '2', 1),
-(103, 'E23-00301', 'Mia', 'Grace', 'Valdez', '', 'BAP', '2', 1),
-(104, 'E23-00302', 'Oliver', 'Jude', 'Torres', '', 'BAP', '2', 1),
-(105, 'E23-00303', 'Chloe', 'Marie', 'Castillo', '', 'BAP', '2', 1),
-(106, 'E23-00304', 'Aiden', 'Jack', 'Fernandez', '', 'BAP', '2', 1),
-(107, 'E24-00300', 'Sarah', 'Anne', 'Lim', '', 'BAP', '1', 1),
-(108, 'E24-00301', 'Daniel', 'Christian', 'Santos', '', 'BAP', '1', 1),
-(109, 'E24-00302', 'Vanessa', 'Joy', 'Morales', '', 'BAP', '1', 1),
-(110, 'E24-00303', 'Kevin', 'Joshua', 'Reyes', '', 'BAP', '1', 1),
-(111, 'E24-00304', 'Emily', 'Grace', 'Cruz', '', 'BAP', '1', 1),
-(112, 'E21-00300', 'Anthony', 'James', 'Castillo', '', 'BASS', '4', 1),
-(113, 'E21-00301', 'Victoria', 'Anne', 'Ramirez', '', 'BASS', '4', 1),
-(114, 'E21-00302', 'Christopher', 'Ryan', 'Mendoza', '', 'BASS', '4', 1),
-(115, 'E21-00303', 'Samantha', 'Joy', 'Villanueva', '', 'BASS', '4', 1),
-(116, 'E21-00304', 'Benjamin', 'Leo', 'Bautista', '', 'BASS', '4', 1),
-(117, 'E22-00300', 'Sophia', 'Marie', 'Morales', '', 'BASS', '3', 1),
-(118, 'E22-00301', 'Liam', 'Joseph', 'Santos', '', 'BASS', '3', 1),
-(119, 'E22-00302', 'Ava', 'Claire', 'Cruz', '', 'BASS', '3', 1),
-(120, 'E22-00303', 'Elijah', 'Thomas', 'Aquino', '', 'BASS', '3', 1),
-(121, 'E22-00304', 'Mia', 'Grace', 'Valdez', '', 'BASS', '3', 1),
-(122, 'E23-00300', 'Oliver', 'David', 'Torres', '', 'BASS', '2', 1),
-(123, 'E23-00301', 'Chloe', 'Elizabeth', 'Fernandez', '', 'BASS', '2', 1),
-(124, 'E23-00302', 'Aiden', 'Jack', 'Lim', '', 'BASS', '2', 1),
-(125, 'E23-00303', 'Emily', 'Kate', 'Reyes', '', 'BASS', '2', 1),
-(126, 'E23-00304', 'Lucas', 'Henry', 'Gonzales', '', 'BASS', '2', 1),
-(127, 'E24-00300', 'Sarah', 'Joy', 'Mendoza', '', 'BASS', '1', 1),
-(128, 'E24-00301', 'Daniel', 'Leo', 'Villanueva', '', 'BASS', '1', 1),
-(129, 'E24-00302', 'Vanessa', 'Claire', 'Cruz', '', 'BASS', '1', 1),
-(130, 'E24-00303', 'Kevin', 'Christian', 'Santos', '', 'BASS', '1', 1),
-(131, 'E24-00304', 'Emma', 'Rose', 'Ramirez', '', 'BASS', '1', 1);
+(1, 'E21-00193', 'Cyanne Justin', 'Labitoria', 'Vega', '', 1, '4', 1);
 
 -- --------------------------------------------------------
 
@@ -421,6 +323,13 @@ ALTER TABLE `candidates`
   ADD PRIMARY KEY (`candidate_id`);
 
 --
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `course_department` (`course_department`);
+
+--
 -- Indexes for table `department`
 --
 ALTER TABLE `department`
@@ -460,7 +369,9 @@ ALTER TABLE `positions`
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course` (`course`),
+  ADD KEY `department` (`department`);
 
 --
 -- Indexes for table `users`
@@ -499,6 +410,12 @@ ALTER TABLE `votes`
 --
 ALTER TABLE `candidates`
   MODIFY `candidate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `department`
@@ -540,7 +457,7 @@ ALTER TABLE `positions`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -569,6 +486,19 @@ ALTER TABLE `votes`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`course_department`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`course`) REFERENCES `course` (`course_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`department`) REFERENCES `department` (`department_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `votes`
