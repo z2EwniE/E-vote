8043d17026b5e06fdcc356c5214c113457a40f83
bitress <byteress@gmail.com>
2024-11-03 23:34:52 +0800

    test

--- admin/process/view-votes-by-id.php
+++ admin/process/view-votes-by-id.php
@@ -7,14 +7,14 @@
 
             try {
 
-                $sql = "SELECT students.*, positions.*,   partylists.*, CONCAT(students.first_name, ' ', students.middle_name, ' ', students.last_name) AS candidate_name  FROM votes 
+                $sql = "SELECT  positions.*,   partylists.*, CONCAT(students.first_name, ' ', students.middle_name, ' ', students.last_name) AS candidate_name  FROM votes 
                             INNER JOIN students ON students.id = votes.student_id
                             INNER JOIN course ON students.course = course.course_id
                             INNER JOIN department ON department.department_id = students.department
                             INNER JOIN candidates ON candidates.candidate_id = votes.candidate_id
                             INNER JOIN positions ON positions.position_id = votes.position_id
                             INNER JOIN partylists ON partylists.partylist_id = votes.partylist_id
-                        WHERE students.student_id = :id ORDER BY votes.voted_at";
+                        WHERE candidates.student_id = :id ORDER BY votes.voted_at";
                 $stmt = $db->prepare($sql);
                 $stmt->bindParam(":id", $student_id);
                 if($stmt->execute()){
